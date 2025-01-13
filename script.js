ca47d9da29de518eebaf2182f21f6ef41f0a6419
Gopal Sharma <gopalsharma456@gmail.com>
2025-01-13 13:49:51 +0530

    github pages based routing updated

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ function checkAuth() {
   const currentPage = window.location.pathname;
 
   if (!auth && currentPage.endsWith("home.html")) {
-    window.location.href = "/auth/login.html";
+    window.location.href = "https://gopalsharma456.github.io/unstop-login-assignment/auth/login.html";
   }
 
   if (auth && currentPage.endsWith("login.html")) {
-    window.location.href = "/home.html";
+    window.location.href = "https://gopalsharma456.github.io/unstop-login-assignment/home.html";
   }
 }
 
